bac5554db07f7b481b9417fe7ee319a0a95eefd2
MaxSamo <62847898+MaxSamo@users.noreply.github.com>
2023-09-25 09:37:45 +0200

    fix: clarify variable names

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn main() -> Result<(), Box<dyn Error>>
     let mut file_voltage = File::open("/sys/class/power_supply/BAT1/voltage_now")?;
     let mut file_current = File::open("/sys/class/power_supply/BAT1/current_now")?;
 
-    let mut voltage: String;
-    let mut current: String;
+    let mut voltage_str: String;
+    let mut current_str: String;
 
     //collected_voltage.reserve(600);
     //collected_current.reserve(600);
@@ -22,16 +22,16 @@ fn main() -> Result<(), Box<dyn Error>>
     {
         file_voltage.seek(SeekFrom::Start(0))?;
         file_current.seek(SeekFrom::Start(0))?;
-        voltage = String::new();
-        current = String::new();
-        file_voltage.read_to_string(&mut voltage)?;
-        file_current.read_to_string(&mut current)?;
-        let voltage_f64 = voltage.trim().parse::<f64>()? / 1_000_000.0;
-        let current_f64 = current.trim().parse::<f64>()? / 1_000_000.0;
+        voltage_str = String::new();
+        current_str = String::new();
+        file_voltage.read_to_string(&mut voltage_str)?;
+        file_current.read_to_string(&mut current_str)?;
+        let voltage_f64 = voltage_str.trim().parse::<f64>()? / 1_000_000.0;
+        let current_f64 = current_str.trim().parse::<f64>()? / 1_000_000.0;
         //collected_current.push(current);
         //collected_voltage.push(voltage);
-        let wattage = voltage_f64 * current_f64;
-        println!("{}",&wattage.to_string());
+        let wattage_f64 = voltage_f64 * current_f64;
+        println!("{}",&wattage_f64.to_string());
         thread::sleep(time::Duration::from_secs(3));
     }
 
