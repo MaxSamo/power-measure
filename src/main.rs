053efd37d6c14e0d3df0127d2186c68bb906f84a
MaxSamo <62847898+MaxSamo@users.noreply.github.com>
2023-10-02 00:27:40 +0200

    fix: add comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,17 @@ fn main() -> Result<(), Box<dyn Error>>
     //let mut collected_voltage: Vec<String> = Vec::new();
     //let mut collected_current: Vec<String> = Vec::new();
 
+    // TODO: automatically detect battery folders
     let mut file_voltage = File::open("/sys/class/power_supply/BAT1/voltage_now")?;
     let mut file_current = File::open("/sys/class/power_supply/BAT1/current_now")?;
 
     let mut voltage_str: String;
     let mut current_str: String;
 
-    //collected_voltage.reserve(600);
+    //collected_voltage.reserve(600);    // may be useful later
     //collected_current.reserve(600);
-    let dataset_size = 600;
+    
+    let dataset_size = 600;    // TODO: set dataset and thread sleep size via CLI parameters
 
     for count in 0..dataset_size
     {
@@ -28,7 +30,7 @@ fn main() -> Result<(), Box<dyn Error>>
         file_current.read_to_string(&mut current_str)?;
         let voltage_f64 = voltage_str.trim().parse::<f64>()? / 1_000_000.0;
         let current_f64 = current_str.trim().parse::<f64>()? / 1_000_000.0;
-        //collected_current.push(current);
+        //collected_current.push(current);    // may be useful later
         //collected_voltage.push(voltage);
         let wattage_f64 = voltage_f64 * current_f64;
         println!("{}",&wattage_f64.to_string());
